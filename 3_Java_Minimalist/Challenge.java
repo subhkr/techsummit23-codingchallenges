a06263e975d123a80dd6ed7d4a7b8ba09d3c1415
Subham Kumar <kumarsub@mediamarktsaturn.com>
2023-07-21 08:38:53 +0000

    Intial solution java challenge.

--- 3_Java_Minimalist/Challenge.java
+++ 3_Java_Minimalist/Challenge.java
@@ -1,7 +1,33 @@
+import java.util.TreeMap;
+import java.util.SortedMap;
+
 public class Challenge {
 
     String weightedSort(String text) {
-        return "";
+        text = text.replaceAll("[,.-?!\\'_-]", "");
+        String[] wordArray = text.split(" ");
+        SortedMap<String, Integer> sortedWeightMap = new TreeMap<String, Integer>();
+        String wString = "";
+        for (String word : wordArray) {
+            if(word.equals("")){
+                continue;
+            }
+            int weight = weight(word);
+            sortedWeightMap.put(word, weight);
+        }
+        for () {
+            int weight = weight(word);
+            wString = word + "|" + weight + "\n" + wString;
+        }
+        return wString;
+    }
+
+    int weight(String word) {
+        int weight = 0;
+        for (char c : word.toCharArray()) {
+            weight += c;
+        }
+        return weight;
     }
 
 }
